7b839e48ebd0a840d31430eb9a219fdfe874cbae
kzlucas <lucas.tesseron@idaos.com>
2021-01-19 17:39:04 +0100

    Various adjustments

--- js/kz_main.js
+++ js/kz_main.js
@@ -1,41 +1,39 @@
 
-window.onload = function() { 
+window.onload = function() {  	
 
-    var isReinit = false; 	
-
-    window.onscroll = function() { reinitCarousel() };
+    reinitCarousel();
+    window.onresize = function() { reinitCarousel() };
 
     function reinitCarousel() {
 
         if (document.body.scrollTop > 1350 || document.documentElement.scrollTop > 1350) {
 
-            if(isReinit == false){
-
-
-                isReinit = true;
-                var owl = jQuery('.owl-carousel');
-                owl.trigger('destroy.owl.carousel');
-                owl.owlCarousel({
-                    loop:true,
-                    items:3,
-                    dots:false,
-                    nav:false,
-                    autoplay:true,
-                    autoplayTimeout:0,
-                    autoplaySpeed:3000,
-                    lazyLoad : true,
-                    onTranslated:next,
-                    responsive : {
-                        768 : {
-                            items : 5
-                        }
+            var owl = jQuery('.owl-carousel');
+            owl.trigger('destroy.owl.carousel');
+            owl.owlCarousel({
+                loop:true,
+                items:2,
+                dots:false,
+                nav:false,
+                autoplay:true,
+                autoplayTimeout:0,
+                autoplaySpeed:3000,
+                lazyLoad : true,
+                onTranslated:next,
+                responsive : {
+                    768 : {
+                        items : 4
+                    },
+                    1200 : {
+                        items : 5
                     }
-                });
-
-                function next(){
-                    owl.trigger('next.owl.carousel', 3000);
                 }
+            });
+
+            function next(){
+                owl.trigger('next.owl.carousel', 3000);
             }
+
         }
     }
 
