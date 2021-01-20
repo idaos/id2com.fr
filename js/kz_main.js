1cb86961fb7613bb93cc6cf857a456e0700c410e
kzlucas <lucas.tesseron@gmail.com>
2021-01-20 18:23:32 +0100

    various fixes

--- js/kz_main.js
+++ js/kz_main.js
@@ -1,45 +1,73 @@
+jQuery( document ).ready(function() {
 
-window.onload = function() {  	
 
-    reinitCarousel();
-    window.onresize = function() { reinitCarousel() };
+    // sticky header
+    // ------------------------
 
-    function reinitCarousel() {
+    jQuery(window).scroll(function() {	
 
-        if (document.body.scrollTop > 1350 || document.documentElement.scrollTop > 1350) {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            jQuery('#header-section').attr('style', 'background-color: #fff !important;');
 
-            var owl = jQuery('.owl-carousel');
-            owl.trigger('destroy.owl.carousel');
-            owl.owlCarousel({
-                loop:true,
-                items:2,
-                dots:false,
-                nav:false,
-                autoplay:true,
-                autoplayTimeout:0,
-                autoplaySpeed:3000,
-                lazyLoad : true,
-                onTranslated:next,
-                responsive : {
-                    768 : {
-                        items : 4
-                    },
-                    1200 : {
-                        items : 5
+        }else{
+            jQuery('#header-section').attr('style', 'background-color: rgba(0,0,0,0) !important;');
+        }
+
+
+
+        // carousel settings
+        // ------------------------
+        isReinit = false;
+        reiitCarousel();
+        window.onresize = function() { reinitCarousel() };
+
+        if(!isReinit){
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+
+                function reinitCarousel() {
+                    isReinit = true;
+                    var owl = jQuery('#section-ref .owl-carousel');
+                    owl.trigger('destroy.owl.carousel');
+                    owl.owlCarousel({
+                        loop:true,
+                        items:2,
+                        dots:false,
+                        nav:false,
+                        autoplay:true,
+                        autoplayTimeout:0,
+                        autoplaySpeed:3000,
+                        lazyLoad : true,
+                        touchDrag  : false,
+                        mouseDrag  : false,
+                        onTranslated:next,
+                        responsive : {
+                            768 : {
+                                items : 4
+                            },
+                            1200 : {
+                                items : 5
+                            }
+                        }
+                    });
+
+                    function next(){
+                        owl.trigger('next.owl.carousel', 3000);
                     }
                 }
-            });
-
-            function next(){
-                owl.trigger('next.owl.carousel', 3000);
             }
-
         }
-    }
+
+    });
+
+    // first word special class in offer section
+    // ------------------------
 
     jQuery('#section-offre h3 > span').each(function(){
         var me = jQuery(this);
         me.html( me.text().replace(/(^\w+)/,'<strong>$1</strong>') );
     });
 
-};
+});
+
+
+
