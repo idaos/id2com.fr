f854e92f2176d1ca6605f17bdacb015181e4e0b0
kzlucas <lucas.tesseron@gmail.com>
2021-01-20 18:53:39 +0100

    carousel fix

--- js/kz_main.js
+++ js/kz_main.js
@@ -12,52 +12,56 @@ jQuery( document ).ready(function() {
         }else{
             jQuery('#header-section').attr('style', 'background-color: rgba(0,0,0,0) !important;');
         }
+    });
 
 
+    // carousel settings
+    // ------------------------
 
-        // carousel settings
-        // ------------------------
-        isReinit = false;
-        reiitCarousel();
-        window.onresize = function() { reinitCarousel() };
+    var to;
+    var owl = jQuery('#section-ref .owl-carousel');
 
-        if(!isReinit){
-            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    owl.on('initialized.owl.carousel', function(event){ 
+        console.log( 'Owl is loaded!' );
+        to = setTimeout( function() {
+            console.log( 'Initializing Owl with custom parameters!' );
+            reinitCarousel();
+        }, 800);
+    });
 
-                function reinitCarousel() {
-                    isReinit = true;
-                    var owl = jQuery('#section-ref .owl-carousel');
-                    owl.trigger('destroy.owl.carousel');
-                    owl.owlCarousel({
-                        loop:true,
-                        items:2,
-                        dots:false,
-                        nav:false,
-                        autoplay:true,
-                        autoplayTimeout:0,
-                        autoplaySpeed:3000,
-                        lazyLoad : true,
-                        touchDrag  : false,
-                        mouseDrag  : false,
-                        onTranslated:next,
-                        responsive : {
-                            768 : {
-                                items : 4
-                            },
-                            1200 : {
-                                items : 5
-                            }
-                        }
-                    });
+    window.onresize = function() { reinitCarousel() };
 
-                    function next(){
-                        owl.trigger('next.owl.carousel', 3000);
-                    }
+
+    function reinitCarousel() {
+
+        owl.trigger('destroy.owl.carousel');
+        owl.owlCarousel({
+            loop:true,
+            items:2,
+            dots:false,
+            nav:false,
+            autoplay:true,
+            autoplayTimeout:0,
+            autoplaySpeed:3000,
+            lazyLoad : true,
+            touchDrag  : false,
+            mouseDrag  : false,
+            onTranslated:next,
+            responsive : {
+                768 : {
+                    items : 4
+                },
+                1200 : {
+                    items : 5
                 }
             }
-        }
+        });
 
-    });
+        function next(){
+            owl.trigger('next.owl.carousel', 3000);
+        }
+        clearTimeout(to);
+    }
 
     // first word special class in offer section
     // ------------------------
